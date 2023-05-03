3bcfb48b0a1223bd918ecc23686cabb41cd8552d
Nokorbis <nokorbis@gmail.com>
2023-05-03 17:49:45 +0200

    Some advancements on the timed editor

--- src/routes/(main)/(auth)/dashboard/game/[id=integer]/prepare/PrepareTimed.svelte
+++ src/routes/(main)/(auth)/dashboard/game/[id=integer]/prepare/PrepareTimed.svelte
@@ -1,6 +1,8 @@
 <script>
-	import { faPlus } from "@fortawesome/free-solid-svg-icons";
+	import { faCircleQuestion, faPlus } from "@fortawesome/free-solid-svg-icons";
 	import Fa from "svelte-fa/src/fa.svelte";
+	import TimedThemeModal from "./TimedThemeModal.svelte";
+	import { modalStore, toastStore } from "@skeletonlabs/skeleton";
 
     export let supabase;
     export let game;
@@ -11,6 +13,46 @@
         return supabase.from('timed_theme')
             .select('id, name, is_mystery, questions:timed_question(*)')
             .eq('game_id', game.id);
+    }
+
+    async function saveTheme(response) {
+        if (response) {
+            console.log({response})
+            const {data: result, error} = await supabase.from('timed_theme')
+                .insert([{user_id: response.user_id, game_key: game.access_key}]);
+
+            if (error) {
+                toastStore.trigger({
+                    message: error.message,
+                    background: 'variant-filled-error'
+                });
+            }
+            else {
+                modalStore.close();
+                gameThemes$ = loadThemes();
+            }
+        }
+        else {
+            toastStore.trigger({
+                message: 'Le thème n\'a pas été sauvegardé',
+                timeout: 2000
+            });
+        }
+    }
+
+    function openThemeEditorModal(theme) {
+        const modalComponent = {
+            ref: TimedThemeModal,
+            props: {
+                supabase,
+                theme
+            }
+        };
+        modalStore.trigger( {
+             type: 'component',
+             component: modalComponent,
+             response: saveTheme
+        });
     }
 </script>
 
@@ -23,9 +65,21 @@
             {#if error}
                 {error.message}
             {:else}
-            <ul>
+            <ul class="grid grid-cols-[max-content_max-content_1fr] gap-2 items-center text-lg mt-2">
                 {#each gameThemes as theme}
-                    <li>{JSON.stringify(theme)}</li>
+                    <li class="contents cursor-pointer" on:click={() => openThemeEditorModal(theme)}>
+                        <div class="justify-self-end">
+                            <span class="badge variant-filled-surface text-base">{theme.questions.length}</span>
+                        </div>
+                        <div>
+                            <span class="badge-icon" title="Thème mystère">
+                                <Fa icon={faCircleQuestion} color="#f2cb00" scale="2x"/>
+                            </span>
+                        </div>
+                        <div>
+                            <span class="grow">{theme.name}</span>
+                        </div>
+                    </li>                    
                 {:else}
                     Aucun thème n'a été trouvé
                 {/each}
@@ -40,5 +94,9 @@
         width: min(50rem, 100% - 1rem);
         margin-inline: auto;
         @apply my-2;
+    }
+
+    li.contents:hover>div {
+        background: #f7f7f7;
     }
 </style>
---
+++ src/routes/(main)/(auth)/dashboard/game/[id=integer]/prepare/TimedThemeModal.svelte
@@ -0,0 +1,35 @@
+<script>
+	import { faTrash } from "@fortawesome/free-solid-svg-icons";
+	import Fa from "svelte-fa/src/fa.svelte";
+
+    export let theme;
+</script>
+
+<div class="card bg-white p-4 h-64 w-[64rem]">
+    <div class="grid grid-cols-[max-content_3fr_max-content_1fr] gap-2 items-center">
+        <label class="label" for="theme-name">Nom : </label>
+        <input id="theme-name" type="text" class="input" bind:value={theme.name}>
+        <label for="mystery-theme">Mystère : </label>
+        <input class="checkbox" type="checkbox" bind:checked={theme.is_mystery}>
+    </div>
+    <div class="mt-4">
+        <div class="">
+            {#each theme.questions as question, i}
+            <div class="input-group input-group-divider grid-cols-[4.5rem_1fr] mt-2">
+                <button 
+                    class="variant-filled-error"
+                    style="justify-content: center;">
+                    <Fa icon={faTrash}/>
+                </button>
+                <input type="text" class="" bind:value={question.statement}>
+                <input type="number" 
+                    style="min-width: 4rem !important; width: 4.5rem; padding-inline-start: 1.25rem"
+                    min=0 max=100 step=1
+                    bind:value={question.points_value}
+                    title="Points par bonne réponse">
+                <input type="text" class="" bind:value={question.answer}>
+            </div>
+            {/each}
+        </div>
+    </div>
+</div>
